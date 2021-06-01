da3a08e084009ad189ba94e7edac93661c1b0f3b
vegorova <vegorova@astralinux.ru>
2021-06-01 18:01:15 +0300

    Test news site added

---
+++ test/testsite/news/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from django.shortcuts import get_object_or_404
+from .models import News, Category
+
+def index(request):
+    # print(request)
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, 'news/index.html', context = context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news,
+                                                  'category': category})
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
